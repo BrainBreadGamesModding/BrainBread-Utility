6bbb057b955c975b96e3e423407303ae4e29db74
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2022-09-03 10:56:14 -0400

    Initial Upload
    Initial Upload

---
+++ BrainBread_Utility.bat
@@ -0,0 +1,27 @@
+@echo off
+
+rem add self updating link here
+
+rem add settings loading and creation
+rem for now that is install location
+
+menu
+
+menu options
+open brainbread
+install brainbread + mods/maps (includes reinstall option)
+uninstall brainbread + mods/maps
+exit
+
+rem download link list
+set steamcmd_link="https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
+set brainbread_1.2_zip="https://github.com/Mythical-Github/brainbread/releases/download/v1.0/brainbread-main.zip"
+
+rem credits list
+people who made brainbread
+steamcmd
+Zpin for brainbreadsteamicon fix
+to do: options for installing and launch mod tools
+
+
+exit /b
