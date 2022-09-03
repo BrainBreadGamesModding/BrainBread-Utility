f087ae65528ee19ea2cf77fcde422aadeaac3bc2
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2022-09-03 12:14:29 -0400

    More Stuff
    More Stuff

--- BrainBread_Utility.bat
+++ BrainBread_Utility.bat
@@ -1,21 +1,22 @@
 @echo off
-color 0A
+color 04
 Title BrainBread Utility
+cd %~dp0
 echo Checking for updates...
 powershell -Command "Start-BitsTransfer -Source "https://github.com/Mythical-Github/BrainBread-Utility/releases/download/vStatic/BrainBread_Utility.bat"
-cls
 if not "%1" == "max" start /MAX cmd /c %0 max & exit/b
-cd %~dp0
 cls
+
+rem download link list
+set steamcmd_link="https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
+set brainbread_1.2_zip="https://github.com/Mythical-Github/brainbread/releases/download/v1.0/brainbread-main.zip"
 
 :utility_setup_check
-cd %~dp0
+if not %scmd_exist% == "true" goto is_steamcmd_installed
 if not %hl_exist% == "true" goto is_half-life_installed
 if not %bb_exist% == "true" goto is_brainbread_installed
-if not %scmd_exist% == "true" goto is_steamcmd_installed
 
-see if brainbread is installed if not see if half life is installed if not see if steamcmd is installed
-check disk space and install anything missing
+rem Check Disk Space Before Installing Anything
 
 :main_menu
 cls
@@ -41,9 +42,37 @@ if %ERRORLEVEL%==2 goto install_brainbread
 if %ERRORLEVEL%==3 goto uninstall_brainbread
 if %ERRORLEVEL%==4 goto exit
 
-
-
 :start_brainbread
+copy "Half-Life\brainbread\cl_dlls\client.dll" "Half-Life\brainbread\cl_dlls\client.dll.bak"
+del /q "Half-Life\brainbread\cl_dlls\client.dll"
+set hl_exe="Half-Life\hl.exe"
+%hl_exe% -game brainbread
+copy "Half-Life\brainbread\cl_dlls\client.dll.bak" "Half-Life\brainbread\cl_dlls\client.dll"
+del /q "Half-Life\brainbread\cl_dlls\client.dll.bak"
+goto exit
+
+:is_steamcmd_installed
+if %scmd_exist% == "false" goto install_steamcmd
+if exist "SteamCMD/steamcmd.exe" set scmd_exist="true"
+if %scmd_exist% == "true" goto utility_setup_check
+
+:is_half-life_installed
+if %hl_exist% =="false" goto install_half-life
+if exist "Half-Life/hl.exe" set hl_exist="true"
+if %hl_exist% =="true" goto utility_setup_check
+
+:is_brainbread_installed
+if %bb_exist% =="false" goto install_brainbread
+if exist "Half-Life/brainbread/brainbread.wad" set bb_exist="true"
+if %bb_exist% =="true" goto utility_setup_check
+
+:install_steamcmd
+
+goto is_steamcmd_installed
+
+:install_half-life
+
+goto is_half-life_installed
 
 :install_brainbread
 
@@ -56,28 +85,7 @@ echo This will uninstall everything in the brainbread folder in your currently s
 echo as well as everything in the folder the bat file is ran from
 echo
 cd %half-life_install%
-if exist "brainbread" del "brainbread"
-
-
-:is_brainbread_installed
-if exist "Half-Life/brainbread/brainbread.wad" set bb_exist="true"
-if %bb_exist% =="true" goto utility_setup_check
-
-:is_half-life_installed
-if exist "Half-Life/hl.exe" set hl_exist="true"
-if %hl_exist% =="true" goto utility_setup_check
-
-:is_steamcmd_installed
-if exist "SteamCMD/steamcmd.exe" set scmd_exist="true"
-if %scmd_exist% == "true" goto utility_setup_check
-
-:install_steamcmd
-
-goto is_steamcmd_installed
-
-:install_half-life
-
-goto is_half-life_installed
+if exist "brainbread" del /s /q "brainbread"
 
 
 
@@ -104,26 +112,14 @@ goto is_half-life_installed
 
 
 
-rem add settings loading and creation
-rem for now that is install location
 
-menu
 
-menu options
-open brainbread
-install brainbread + mods/maps (includes reinstall option)
-uninstall brainbread + mods/maps
-exit
 
-rem download link list
-set steamcmd_link="https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
-set brainbread_1.2_zip="https://github.com/Mythical-Github/brainbread/releases/download/v1.0/brainbread-main.zip"
 
 rem credits list
 people who made brainbread
 steamcmd
 Zpin for brainbreadsteamicon fix
 to do: options for installing and launch mod tools
-
 
 exit /b
